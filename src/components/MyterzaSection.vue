e29c31ab26f52d029d1b1f1d868cd665dcb8ba07
saidmoussadek <saiddus24@gmail.com>
2022-09-07 14:03:47 +0200

    push

--- src/components/MyterzaSection.vue
+++ src/components/MyterzaSection.vue
@@ -3,20 +3,25 @@
     <div class="container">
      <span>OUR BUSINESS AREAS</span>
      <h3>Excellence in Services</h3>
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti aut odio temporibus sed nostrum libero quisquam doloribus ad laudantium nulla quaerat ab quidem porro tempora culpa adipisci, fuga error quis?</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti aut odio temporibus sed nostrum libero quisquam doloribus ad laudantium nulla quaerat <br> ab quidem porro tempora culpa adipisci, fuga error quis?</p>
+     <span>SELL ALL</span>
+      </div>
       <div class="containerCard">
           <div class="card" v-for="(card , index) in cards" :key="index" :cards="cards">
           <span><i :class= card.icone></i></span>
           <span><i class="fa-solid fa-arrow-right"></i></span>
-          <h4>{{card.titolo}}</h4>
-          <p>{{card.text}}</p>
+          <div class="desriptionCard">
+            <h4>{{card.titolo}}</h4>
+            <p>{{card.text}}</p>
+          </div>
+          
         </div>
 
       </div>
     
      
 
-    </div>
+    
 
   </section>
 </template>
@@ -37,10 +42,39 @@ export default {
 
 .terza{
   border:2px solid red;
+  background-color: rgb(17, 17, 23);
+   .container{
+     padding:2rem 3rem ;
+     border: 2px solid red;
+     span{
+      color:rgb(0, 171, 167) ;
+     }
+     h3{
+       color:white;
+       }
+     P{
+      color: rgb(189, 190, 180);
+      display: inline;
+       
+     
+      
+      
+     }
+   }
+
  
   .containerCard{
     display: flex;
     flex-wrap: wrap;
+    .desriptionCard{
+          padding: 1rem 2rem 0 0;
+          color:rgb(253, 252, 250);
+          p{
+            margin-top: 1rem;
+            color: rgb(189, 190, 180)
+          }
+
+        }
     .card{
       width: calc(100% / 3 - 5rem);
       height:200px;
@@ -49,12 +83,14 @@ export default {
       margin-bottom:2rem;
       margin-top:2rem;
       border: 2px solid ;
+      background-color: rgb(25, 25, 31);
       span{
         font-size: 1.5rem;
         color:rgb(3, 135, 134);
         .fa-arrow-right{
           margin-left:11rem ;
         }
+        
       }
     }
   }
